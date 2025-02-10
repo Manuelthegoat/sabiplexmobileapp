3e4cf7c45793dfd57592dd3f9b251e16cce56633
manuelthegoat <manuelcreation247@gmail.com>
2025-02-10 15:05:19 +0100

    Onboarding Screen (next, login)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,35 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { SplashScreen, Stack } from 'expo-router'
+import { ThemeProvider } from '@/context/theme.context'
+import {
+  Poppins_600SemiBold,
+  Poppins_300Light,
+  Poppins_400Regular,
+  Poppins_700Bold,
+  Poppins_500Medium,
+  useFonts,
+} from "@expo-google-fonts/poppins";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+//  SplashScreen.preventAutoHideAsync();
 
 export default function _layout() {
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    Poppins_600SemiBold,
+    Poppins_300Light,
+    Poppins_700Bold,
+    Poppins_400Regular,
+    Poppins_500Medium,
+  });
+
   return (
-    <View>
-      <Text>_layout</Text>
-    </View>
+   <ThemeProvider>
+    <Stack screenOptions={{headerShown: false}}>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="(routes)/onboarding/index" />
+    </Stack>
+   </ThemeProvider>
   )
 }
